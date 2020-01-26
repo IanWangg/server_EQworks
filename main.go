1ed04ee658c3ecd7436f453acb58f872ad347a91
IanWangg <43913458+IanWangg@users.noreply.github.com>
2020-01-26 00:57:50 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+type counters struct {
+	sync.Mutex
+	view  int
+	click int
+}
+
+var (
+	c = counters{}
+
+	content = []string{"sports", "entertainment", "business", "education"}
+)
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to EQ Works 😎")
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	data := content[rand.Intn(len(content))]
+
+	c.Lock()
+	c.view++
+	c.Unlock()
+
+	err := processRequest(r)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(400)
+		return
+	}
+
+	// simulate random click call
+	if rand.Intn(100) < 50 {
+		processClick(data)
+	}
+
+	fmt.Println("Key:", data, ":", time.Now(), "      ")
+	fmt.Println("Value:{views:", c.view, "clicks:", c.click, "}")
+}
+
+func processRequest(r *http.Request) error {
+	time.Sleep(time.Duration(rand.Int31n(50)) * time.Millisecond)
+	return nil
+}
+
+func processClick(data string) error {
+	c.Lock()
+	c.click++
+	c.Unlock()
+
+	return nil
+}
+
+func statsHandler(w http.ResponseWriter, r *http.Request) {
+	if !isAllowed() {
+		w.WriteHeader(429)
+		return
+	}
+}
+
+func isAllowed() bool {
+	return true
+}
+
+func uploadCounters(f *os.File) error {
+	for isAllowed(){
+		time.Sleep(5 * time.Second)
+		//messenge := "{views:" + string(c.view) + "clicks:" + string(c.click) + "}"
+
+		fmt.Fprintln(f, "{views:" , c.view , "clicks:" , c.click , "}")
+	}
+	return nil
+}
+
+func main() {
+	f, err := os.Create("MockStore.txt")
+	if err != nil {
+		panic(err)
+	}
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/stats/", statsHandler)
+	go uploadCounters(f)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
